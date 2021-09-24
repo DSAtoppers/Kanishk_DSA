0d1f6b1cd5da515dc2b4963a38159a3d92bdfdd5
homide <chauhankanishk990@gmail.com>
2021-09-24 22:19:14 +0530

    24th September 2021 - Topic: Strings

--- src/sept2421/Code1078.java
+++ src/sept2421/Code1078.java
@@ -1,4 +1,28 @@
 package sept2421;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Code1078 {
+
+    public static String[] findOcurrences(String text, String first, String second) {
+        String[] arr = text.split("\\s+");
+        ArrayList<String> arrayList = new ArrayList<>();
+        int i = 0;
+        while (i <= arr.length-3){
+            if (arr[i].equals(first) && arr[i+1].equals(second)){
+                arrayList.add(arr[i+2]);
+            }
+            i++;
+        }
+
+        String[] main = new String[arrayList.size()];
+        return arrayList.toArray(main);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findOcurrences("ypkk lnlqhmaohv lnlqhmaohv lnlqhmaohv ypkk ypkk ypkk ypkk ypkk ypkk lnlqhmaohv lnlqhmaohv lnlqhmaohv lnlqhmaohv ypkk ypkk ypkk lnlqhmaohv lnlqhmaohv ypkk",
+                "lnlqhmaohv",
+                "ypkk")));
+    }
 }
